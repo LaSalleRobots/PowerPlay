62139b793579a579a25b935236ca05785088943f
Lukas Werner <lwerner@lshigh.org>
2022-11-10 16:15:21 -0800

    Update AutoWithEncodersLeft w/ encoders goDist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithEncodersLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithEncodersLeft.java
@@ -15,6 +15,9 @@ public class AutoWithEncodersLeft extends LinearOpMode {
 
         Vision vision = new Vision(hardwareMap);
 
+        double ticksPerIn = 40.887;
+        double inchesPerBox = 21.375; // 24 for meet
+
         while (opModeInInit()) {
             int id = vision.getIdentifier();
             telemetry.addData("Id:", id);
@@ -25,25 +28,60 @@ public class AutoWithEncodersLeft extends LinearOpMode {
 
         robot.grabber.close();
         robot.sleep(0.5);
-        robot.lift.setPosition(80);
+        robot.lift.setPosition(robot.lift.SMALL);
 
-        int id = vision.getIdentifier();
-        telemetry.addData("Id:", id);
-        telemetry.update();
-
-        robot.lift.setPosition(125);
-
-        robot.drive.runToPosition(98, 97, 106, 105)
-                .runToPosition(999, 945, -863, -886);
-        robot.lift.setPosition(3010);
+        robot.drive.forward().goDist(1 * ticksPerIn);
         robot.sleep(0.5);
-        robot.drive.runToPosition(2612, 2601, 788, 801)
-                .runToPosition(2873, 2347, 1099, 577);
 
-        robot.drive.runToPosition(3297, 2750, 1523, 986);
+        robot.drive.right().goDist(inchesPerBox * ticksPerIn);
+        robot.sleep(0.5);
+
+        robot.drive.forward().goDist(2 * inchesPerBox * ticksPerIn);
+        robot.sleep(0.5);
+
+        robot.drive.left().goDist(0.5 * inchesPerBox * ticksPerIn);
+        robot.sleep(0.5);
+
+        robot.lift.setPosition(robot.lift.LARGE);
+        robot.sleep(0.5);
+
+        robot.drive.forward().goDist(3 * ticksPerIn);
+        robot.sleep(0.5);
+
         robot.grabber.open();
-        robot.sleep(.5);
-        robot.lift.setPosition(125);
+        robot.sleep(0.5);
+
+        robot.drive.backward().goDist(3 * ticksPerIn);
+        robot.sleep(0.5);
+
+        robot.lift.setPosition(robot.lift.SMALL);
+        robot.sleep(0.5);
+
+        robot.drive.left().goDist(1.5 * inchesPerBox * ticksPerIn);
+        robot.sleep(0.5);
+
+
+//        robot.grabber.close();
+//        robot.sleep(0.5);
+//        robot.lift.setPosition(80);
+//
+//        int id = vision.getIdentifier();
+//        telemetry.addData("Id:", id);
+//        telemetry.update();
+//
+//        robot.lift.setPosition(125);
+//
+//        robot.drive.runToPosition(98, 97, 106, 105)
+//                .runToPosition(999, 945, -863, -886);
+//        robot.lift.setPosition(3010);
+//        robot.sleep(0.5);
+//        robot.drive.runToPosition(2612, 2601, 788, 801)
+//                .runToPosition(2873, 2347, 1099, 577);
+//
+//        robot.drive.runToPosition(3297, 2750, 1523, 986);
+//        robot.grabber.open();
+//        robot.sleep(.5);
+//        robot.lift.setPosition(125);
 
 
     }
